86c33bfe3553d11ad2e04894c1d63805418010ea
Adrian Abreu <me@adrianabreu.com>
2017-11-06 23:12:27 +0100

    Extracted reducers to a new function, fix #18

--- src/store/Historical.ts
+++ src/store/Historical.ts
@@ -51,68 +51,82 @@ export const actionCreators = {
 };
 
 // REDUCERS
+
+function addSentence(state: HistoricalState, action: AddSentenceAction) {
+    let sentences = [...state.sentences, action.sentence];
+    let sentenceCategories: Filter[] = action.sentence.categories.map(category => {
+        return {
+            value: category,
+            selected: false
+        };
+    });
+    const newCategories = sentenceCategories.filter(category => state.historicalFilter.filter(
+        historicalCategory => category.value === historicalCategory.value
+    ).length === 0);
+
+    const newFilters = [...state.historicalFilter, ...newCategories];
+
+    const toggledFilters = state.historicalFilter.filter(filter => filter.selected);
+
+    const visibleSentences = toggledFilters.length ? state.sentences.filter(sentence => {
+        return sentence.categories
+            .filter(sentenceCat =>
+                toggledFilters.map(filter => filter.value)
+                    .indexOf(sentenceCat))
+            .length;
+    }) : [...sentences];
+
+    return {
+        historicalFilter: newFilters,
+        sentences,
+        visibleSentences: visibleSentences
+    };
+}
+
+function toggleHistoricalFilter(state: HistoricalState, action: KnownAction) {
+    const filters = [...state.historicalFilter];
+    filters[action.index].selected = !filters[action.index].selected;
+    const toggledFilters = filters.filter(filter => filter.selected);
+    const visibleSentences = toggledFilters.length ? state.sentences.filter(sentence => {
+        return sentence.categories
+            .filter(sentenceCat => {
+                return toggledFilters.map(filter => filter.value)
+                    .indexOf(sentenceCat) !== -1;
+            }
+            ).length === toggledFilters.length;
+    }) : [...state.sentences];
+    return {
+        sentences: state.sentences,
+        historicalFilter: filters,
+        visibleSentences
+    };
+}
+
+function clearHistoricalFilters(state: HistoricalState, action: KnownAction) {
+    const untoggledFilters: Filter[] = state.historicalFilter.map(filter => {
+        return {
+            value: filter.value,
+            selected: false
+        };
+    });
+    return {
+        sentences: [...state.sentences],
+        historicalFilter: untoggledFilters,
+        visibleSentences: [...state.sentences]
+    };
+}
+
 export const reducer: Reducer<HistoricalState> = (state: HistoricalState, action: KnownAction) => {
 
     switch (action.type) {
-        case 'ADD_SENTENCE':
-            let sentences = [...state.sentences, action.sentence];
-            let sentenceCategories: Filter[] = action.sentence.categories.map(category => {
-                return {
-                    value: category,
-                    selected: false
-                };
-            });
-            const newCategories = sentenceCategories.filter(category => state.historicalFilter.filter(
-                historicalCategory => category.value === historicalCategory.value
-            ).length === 0);
-            const newFilters = [...state.historicalFilter, ...newCategories];
-            const tF = state.historicalFilter.filter(filter => filter.selected);
-            const vS = tF.length ? state.sentences.filter(sentence => {
-                return sentence.categories
-                    .filter(sentenceCat =>
-                        toggledFilters.map(filter => filter.value)
-                            .indexOf(sentenceCat))
-                    .length;
-            }) : [...sentences];
-            return {
-                historicalFilter: newFilters,
-                sentences,
-                visibleSentences: vS
-            };
-        case 'TOGGLE_HISTORICAL_FILTER':
-            const filters = [...state.historicalFilter];
-            filters[action.index].selected = !filters[action.index].selected;
-            const toggledFilters = filters.filter(filter => filter.selected);
-            const visibleSentences = toggledFilters.length ? state.sentences.filter(sentence => {
-                return sentence.categories
-                    .filter(sentenceCat => {
-                        return toggledFilters.map(filter => filter.value)
-                            .indexOf(sentenceCat) !== -1;
-                    }
-                    ).length === toggledFilters.length;
-            }) : [...state.sentences];
-            return {
-                sentences: state.sentences,
-                historicalFilter: filters,
-                visibleSentences
-            };
+        case 'ADD_SENTENCE': return addSentence(state, action);
+        case 'TOGGLE_HISTORICAL_FILTER': return toggleHistoricalFilter(state, action);
+        case 'CLEAR_HISTORICAL_FILTERS': return clearHistoricalFilters(state, action);
         case 'CLEAR_HISTORICAL':
             return {
                 sentences: [],
                 historicalFilter: [],
                 visibleSentences: []
-            };
-        case 'CLEAR_HISTORICAL_FILTERS':
-            const untoggledFilters: Filter[] = state.historicalFilter.map(filter => {
-                return {
-                    value: filter.value,
-                    selected: false
-                };
-            });
-            return {
-                sentences: [...state.sentences],
-                historicalFilter: untoggledFilters,
-                visibleSentences: [...state.sentences]
             };
         default:
             return state || { sentences: [], visibleSentences: [], historicalFilter: [] };
